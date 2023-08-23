98327f911aed56ec1e028e666a09299925995636
Utsadawut <utsadawut.sangthong@gmail.coom>
2023-08-23 22:11:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<navbar_product />
+ 
+  <div class="container">
+    <router-view />
+
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import navbar_product from './components/navbar_product.vue';
+import navbar_productVue from './components/navbar_product.vue'
+
+import { useProductStore } from './store/product'
+const product_store = useProductStore()
+
+import  { useCartStore } from './store/cart';
+const cart_store = useCartStore()
+
+onMounted(()=> product_store.fetch_products())
+onMounted(()=> cart_store.loadFromLocalStrorge())
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/navbar_product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg" style="background-color: #ffc107;">
+  <div class="container-fluid">
+    <a class="navbar-brand" ><i class="fas fa-book-reader me-2"></i>RURUShop</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <routerLink to="/" class="nav-link active" aria-current="page" href="#">หน้าแรก</routerLink>
+        </li>
+        <li class="nav-item">
+          <routerLink to="/cart" class="nav-link active" aria-current="page" href="#">ตระกร้าสินค้า</routerLink>
+        </li>
+      </ul>
+      <div class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="ค้นหาเลย.." aria-label="Search" v-model="product_store.input">
+      </div>
+    </div>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script setup>
+import { useProductStore } from '../store/product'
+const product_store = useProductStore()
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
